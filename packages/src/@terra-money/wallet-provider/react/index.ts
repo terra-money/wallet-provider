c850a5c42ec9f5542fc613e3a3d45917780ac5ec
SSen <i@ssen.name>
2021-06-21 16:43:41 +0900

    fix: remove react-router-dom depends functions

--- packages/src/@terra-money/wallet-provider/react/index.ts
+++ packages/src/@terra-money/wallet-provider/react/index.ts
@@ -1,5 +1,4 @@
 export * from './ExtensionNetworkOnlyWalletProvider';
-export * from './useRouterWalletStatusRecheck';
 export * from './useConnectedWallet';
 export * from './useWallet';
 export * from './WalletProvider';
