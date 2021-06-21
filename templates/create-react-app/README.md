53529ba5edd34d6b2ffe64e0ee39d789c88f0b7a
SSen <i@ssen.name>
2021-06-21 16:36:00 +0900

    feat: add CRA template

---
+++ templates/create-react-app/README.md
@@ -0,0 +1,8 @@
+# How to use this template
+
+```sh
+npx copy-github-directory https://github.com/terra-money/wallet-provider/tree/main/templates/create-react-app your-app-name
+cd your-app-name
+yarn install
+yarn start
+```
---
+++ templates/create-react-app/src/components/TxSample.tsx
@@ -0,0 +1,89 @@
+import { MsgSend } from '@terra-money/terra.js';
+import {
+  CreateTxFailed,
+  Timeout,
+  TxFailed,
+  TxResult,
+  TxUnspecifiedError,
+  useConnectedWallet,
+  UserDenied,
+} from '@terra-money/wallet-provider';
+import React, { useCallback, useState } from 'react';
+
+const toAddress = 'terra12hnhh5vtyg5juqnzm43970nh4fw42pt27nw9g9';
+
+export function TxSample() {
+  const [txResult, setTxResult] = useState<TxResult | null>(null);
+  const [txError, setTxError] = useState<string | null>(null);
+
+  const connectedWallet = useConnectedWallet();
+
+  const send = useCallback(() => {
+    if (!connectedWallet) {
+      return;
+    }
+
+    if (!connectedWallet.network.chainID.startsWith('tequila')) {
+      console.error(`Please only execute this example on Testnet`);
+      return;
+    }
+
+    setTxResult(null);
+
+    connectedWallet
+      .post({
+        msgs: [
+          new MsgSend(connectedWallet.walletAddress, toAddress, {
+            uusd: 100000,
+          }),
+        ],
+      })
+      .then((txResult: TxResult) => {
+        console.log(txResult);
+        setTxResult(txResult);
+      })
+      .catch((error: unknown) => {
+        if (error instanceof UserDenied) {
+          setTxError('User Denied');
+        } else if (error instanceof CreateTxFailed) {
+          setTxError('Create Tx Failed: ' + error.message);
+        } else if (error instanceof TxFailed) {
+          setTxError('Tx Failed: ' + error.message);
+        } else if (error instanceof Timeout) {
+          setTxError('Timeout');
+        } else if (error instanceof TxUnspecifiedError) {
+          setTxError('Unspecified Error: ' + error.message);
+        } else {
+          setTxError(
+            'Unknown Error: ' +
+              (error instanceof Error ? error.message : String(error)),
+          );
+        }
+      });
+  }, [connectedWallet]);
+
+  return (
+    <div>
+      <h1>Tx Sample</h1>
+      {connectedWallet?.availablePost && !txResult && !txError && (
+        <button onClick={send}>Send 1USD to {toAddress}</button>
+      )}
+      {txResult && (
+        <>
+          <pre>{JSON.stringify(txResult, null, 2)}</pre>
+          <button onClick={() => setTxResult(null)}>Clear Tx Result</button>
+        </>
+      )}
+      {txError && (
+        <>
+          <pre>{txError}</pre>
+          <button onClick={() => setTxError(null)}>Clear Tx Error</button>
+        </>
+      )}
+      {!connectedWallet && <p>Wallet not connected!</p>}
+      {connectedWallet && !connectedWallet.availablePost && (
+        <p>Can not post Tx</p>
+      )}
+    </div>
+  );
+}
